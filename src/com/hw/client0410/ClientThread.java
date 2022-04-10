ac51c5f59a61a33e4ef6023dbd6a297bfec28a56
Evan <761356554@qq.com>
2022-04-10 20:27:42 +0800

    Updated

--- src/com/hw/client0410/ClientThread.java
+++ src/com/hw/client0410/ClientThread.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.net.Socket;
 
 //这是专门为客户端写的接收其他客户端传来的消息的线程
-public class ClientThread implements Runnable {
+public class ClientThread implements Runnable, MsgType{
     private Socket s;
     private JTextArea area;
     private DefaultListModel<String> userList;
@@ -22,12 +22,12 @@ public class ClientThread implements Runnable {
             int head = input.read(msgBytes);
             System.out.println("得到消息协议头，未确认该消息头具体内容!");
             switch (head) {
-                case 1:
+                case GROUP:
                     GroupChatThread gct = new GroupChatThread(s, area);
                     Thread t1 = new Thread(gct);
                     t1.start();
                     break;
-                case 3:
+                case USER:
                     System.out.println("已确认收到的消息协议为用户上线消息");
                     byte[] userBytes = new byte[30];
                     int len = input.read(userBytes);
