365b9cb65585677ab9df4ab7fa6c473ab3d4c83e
Evan <761356554@qq.com>
2022-04-10 19:11:52 +0800

    Update MsgType.java

--- src/com/hw/client0410/MsgType.java
+++ src/com/hw/client0410/MsgType.java
@@ -3,5 +3,5 @@ package com.hw.client0410;
 public interface MsgType {
     public final byte GROUP = 1;  //群聊
     public final byte PRIVATE = 2;  //私聊
-    public final byte User = 3;  //用户上线
+    public final byte USER = 3;  //用户上线
 }
