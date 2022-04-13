b9c70bae13fd9fb4493950e4f65a0ed04b26f6cc
Evan <761356554@qq.com>
2022-04-13 19:50:04 +0800

    Update

--- src/com/hw/client0410/ClientThread.java
+++ src/com/hw/client0410/ClientThread.java
@@ -10,6 +10,7 @@ public class ClientThread implements Runnable, MsgType {
     private JTextArea area;
     private DefaultListModel<String> userModel;
     private JList<String> userList;
+    private String[] userArr = new String[1024];
     public ClientThread(Socket s, JTextArea area, DefaultListModel<String> userModel, JList<String> userList) {
         this.s = s;
         this.area = area;
@@ -27,9 +28,9 @@ public class ClientThread implements Runnable, MsgType {
             int len = input.read(userBytes);
             String userMsg = new String(userBytes, 0, len);
             System.out.println("用户上线消息收到，准备添加进列表...");
-            userModel.addElement(userMsg);
-            userList.setModel(userModel);
+            userArr[i] = userMsg;
         }
+        userList.setListData(userArr);
     }
 
     public void run() {
