bfd6090f0e31894f20bd66bae3372ec9b830df10
Evan <761356554@qq.com>
2022-04-17 21:30:26 +0800

    Update

--- src/com/hw/client0410/Client.java
+++ src/com/hw/client0410/Client.java
@@ -20,6 +20,7 @@ public class Client implements ActionListener, ListSelectionListener {
     private JPanel textPane, rightPanel, pFieldPane;
     private JButton sendButton, pSendButton;
     private JList<String> userList;
+
     
     public void createClient(String ip, int port) {
         try {
