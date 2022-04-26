5eb3e2c0129e39311393ef56083812bf09e7eb20
Evan <761356554@qq.com>
2022-04-26 13:57:12 +0800

    Updates

--- src/com/hw/client0410/Login.java
+++ src/com/hw/client0410/Login.java
@@ -2,8 +2,10 @@ package com.hw.client0410;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login {
+public class Login implements ActionListener {
     public void initUI() {
         JFrame jf = new JFrame("登录");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -13,16 +15,28 @@ public class Login {
         //用户名与文本框部分
         JPanel textPanel = new JPanel();
         JTextField userArea = new JTextField();
+        userArea.setPreferredSize(new Dimension(350, 30));
         JTextField passwordArea = new JTextField();
-
+        passwordArea.setPreferredSize(new Dimension(350, 30));
+        textPanel.add(userArea);
+        textPanel.add(passwordArea);
 
         //按钮部分
         JPanel buttonPanel = new JPanel();
+        JButton loginButton = new JButton("Login");
+        JButton registerButton = new JButton("Register");
+        buttonPanel.add(loginButton);
+        buttonPanel.add(registerButton);
 
         //最后完成整体界面
         jf.add(textPanel, BorderLayout.CENTER);
         jf.add(buttonPanel, BorderLayout.SOUTH);
         jf.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
     public static void main(String[] args) {
