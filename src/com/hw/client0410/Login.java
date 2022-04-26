9d1f4c1cdb60512ab765ce5205bc326246d027c8
Evan <761356554@qq.com>
2022-04-26 09:56:24 +0800

    update

--- src/com/hw/client0410/Login.java
+++ src/com/hw/client0410/Login.java
@@ -1,17 +1,29 @@
 package com.hw.client0410;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login {
     public void initUI() {
         JFrame jf = new JFrame("登录");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLocationRelativeTo(null);
         jf.setSize(400, 400);
 
+        //用户名与文本框部分
+        JPanel textPanel = new JPanel();
+        JTextField userArea = new JTextField();
+
+        //按钮部分
+        JPanel buttonPanel = new JPanel();
+
+        //最后完成整体界面
+        jf.add(textPanel, BorderLayout.CENTER);
+        jf.add(buttonPanel, BorderLayout.SOUTH);
         jf.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        new Login().initUI();
     }
 }
